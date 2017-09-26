777ff32319239159f86c7d944a873409f285573a
Oxyrus <im.oxyrus@gmail.com>
2017-09-25 21:39:59 -0500

    Start working on views and controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,27 @@
 class TicketsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @tickets = current_user.tickets.order("created_at DESC")
+  end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.create(ticket_params)
+    @ticket.user = current_user
+
+    if @ticket.save
+      redirect_to tickets_path, :notice => "Ticket submitted!"
+    else
+      render "new"
+    end
+  end
+
+  private
+    def ticket_params
+      params.require(:ticket).permit(:description)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926020323) do
+ActiveRecord::Schema.define(version: 20170926021543) do
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20170926020323) do
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["name"], name: "index_roles_on_name"
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
